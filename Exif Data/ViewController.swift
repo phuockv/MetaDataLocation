538585c0b5513df691794ed40d64dc3a9ac47b11
Vuong Toan Chung <chungvtgc00989@gmail.com>
2019-08-20 08:57:05 +0700

    Initial Commit

---
+++ Exif Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exif Data
+//
+//  Created by Vuong Toan Chung on 8/20/19.
+//  Copyright © 2019 Vuong Toan Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ Exif DataTests/Exif_DataTests.swift
@@ -0,0 +1,34 @@
+//
+//  Exif_DataTests.swift
+//  Exif DataTests
+//
+//  Created by Vuong Toan Chung on 8/20/19.
+//  Copyright © 2019 Vuong Toan Chung. All rights reserved.
+//
+
+import XCTest
+@testable import Exif_Data
+
+class Exif_DataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
